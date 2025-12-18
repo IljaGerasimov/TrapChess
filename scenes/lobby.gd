0003845b02b280debfcec919f0cc256dcb8ef3fa
IljaGerasimov <geras.ilja@gmail.com>
2025-12-18 17:50:40 +0100

    added simple networking code

--- scenes/lobby.gd
+++ scenes/lobby.gd
@@ -9,7 +9,7 @@ signal join_game_pressed(address_text, name_text)
 
 # ---VARIABLES----------------------------------------------
 
-# @onready only initialises if the node finished loading
+# @onready only initializes if the node finished loading
 # (var: void *) variable that points to an object in this case
 # $: find this child in the scene-tree, must be same path as in scene dock
 @onready var Address_Input = $CenterContainer/VBoxContainer/AddressInput
@@ -19,6 +19,7 @@ signal join_game_pressed(address_text, name_text)
 
 # func: like a void function
 # _on_...: signaled functions via Node-Dock
+# host function
 func _on_host_button_pressed():
 	if Name_Input.text == "":
 		Name_Input.text = "HostPlayer"
@@ -26,6 +27,7 @@ func _on_host_button_pressed():
 	# emit_signal: calls all functions that wait for this signal
 	host_game_pressed.emit(Name_Input.text)
 
+# join function
 func _on_join_button_pressed():
 	if Name_Input.text == "":
 		Name_Input.text = "GuestPlayer"
--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,60 @@
-extends Control
+extends Node
 
+# const: like #define or const int in c
+const PORT = 7000
+const MAX_CLIENTS = 2
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+# dereferencing the Lobby-Node
+@onready var lobby = $Lobby
 
+# --- MAIN FUNCTIONS----------------------------------------------
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# func _ready(): like the main() in C but sepperately for each Object
+# Called only once when objects scene loads
+func _ready():
+	#if Lobby sends a signal execute functions _on_...
+	lobby.host_game.connect(_on_host_game)
+	lobby.join_game.connect(_on_join_game)
+
+# --- HOST LOGIC----------------------------------------------
+
+func _on_host_game(player_name):
+	# malloc/new networking-object
+	var peer = ENetMultiplayerPeer.new()
+
+	# test Server Connection (return Error code)
+	var error = peer.create_server(PORT, MAX_CLIENTS)
+	if error != OK: #OK: Enum for Value 0
+		print("Error: " + str(error))
+		return
+	
+	# global Network pointer
+	multiplayer.multiplayer_peer = peer
+	print("Server established by " + player_name + "! Waiting for friend ...")
+	
+	# Wait for client response (register callback)
+	# if connection succeeds -> call func (_on_player_connected)
+	multiplayer.peer_connected.connect(_on_player_connected)
+
+# ---CLIENT LOGIC----------------------------------------------
+
+func _on_join_game(address, player_name):
+	var peer = ENetMultiplayerPeer.new()
+
+	if address == "":
+		address = "127.0.0.1" # Standard Localhost-IP (IPv4)
+	
+	peer.create_client(address, PORT)
+	
+	# Check if connection was built
+	if peer.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		print("Connection faltered!")
+		return
+
+	multiplayer.multiplayer_peer = peer
+	print("Connecting" + player_name + "to server ...")
+
+# ---CALLBACKS----------------------------------------------
+func _on_player_connected(id):
+	# id: unique player integer
+	print("New player connected: " + str(id))
